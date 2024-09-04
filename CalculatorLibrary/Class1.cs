e23637b4495316dd566b55909154c926e04c5377
Jean-David Pallares <2331153@champlaincollege.qc.ca>
2024-09-04 10:38:44 -0400

    Test comment

---
+++ CalculatorLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class Class1
+    {
+        public static decimal add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+    }
+}
